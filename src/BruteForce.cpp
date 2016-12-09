adadab2b9853716a45a030779501c2865c8ac3cc
zyin <zyin@zyindeMacBook-Pro.local>
2016-12-09 19:38:19 +0800

    add BruteForce method

--- src/BruteForce.cpp
+++ src/BruteForce.cpp
@@ -1,5 +1,42 @@
 #include "BruteForce.h"
+#include "string.h"
+#include "stdio.h"
 
 int BruteForce::match(const char *text, const char *pattern) {
+	printf(">>>text:%s<<<<\n", text);
+	printf(">>>pattern:%s<<<<\n", pattern);
+	if (*text == '/0' || *pattern == '/0') {
+		return -1;
+	}
+	int tLen = strlen(text);
+	int pLen = strlen(pattern);
+	printf(">>>text:%d<<<<\n", tLen);
+	printf(">>>pattern:%d<<<<\n", pLen);
+	if (tLen < pLen) {
+		return -1;
+	}
+
+	const char *p = pattern;
+	const char *b = text;
+	const char *t = b;
+
+	while (*p != '\0') {
+		printf(">>>p:%s<<<<\n", p);
+		printf(">>>t:%s<<<<\n", t);
+		printf(">>>pattern:%s<<<<\n", pattern);
+		printf(">>>text:%s<<<<\n", text);
+
+		if (*p == *t) {
+			p++;
+			t++;
+		} else {
+			p = pattern;
+			b++;
+			t = b;
+		}
+		if (*p == '\0') {
+			return (t - text) - (p - pattern);
+		}
+	}
 	return -1;
 }
--- src/StringMatchTest.cpp
+++ src/StringMatchTest.cpp
@@ -3,7 +3,7 @@
 
 TEST(StringMatchTest, should_return_right_position_when_match_pattern_str_by_brute_force_method){
 	BruteForce bf;
-	char *text = "HERE IS A SIMPLE EXAMPLE", *pattern = "EXAMPLE";
+	char text[] = "HERE IS A SIMPLE EXAMPLE", pattern[] = "EXAMPLE";
 
 	ASSERT_EQ(17, bf.match(text, pattern));
 }
