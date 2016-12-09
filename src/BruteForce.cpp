a810d5de90a4c245d1cb667c3953fb1466f87e50
zyin <zyin@zyindeMacBook-Pro.local>
2016-12-09 19:51:56 +0800

    remove print msg

--- src/BruteForce.cpp
+++ src/BruteForce.cpp
@@ -1,17 +1,13 @@
 #include "BruteForce.h"
 #include "string.h"
-#include "stdio.h"
 
 int BruteForce::match(const char *text, const char *pattern) {
-	printf(">>>text:%s<<<<\n", text);
-	printf(">>>pattern:%s<<<<\n", pattern);
 	if (*text == '\0' || *pattern == '\0') {
 		return -1;
 	}
+
 	int tLen = strlen(text);
 	int pLen = strlen(pattern);
-	printf(">>>text:%d<<<<\n", tLen);
-	printf(">>>pattern:%d<<<<\n", pLen);
 	if (tLen < pLen) {
 		return -1;
 	}
@@ -19,13 +15,7 @@ int BruteForce::match(const char *text, const char *pattern) {
 	const char *p = pattern;
 	const char *b = text;
 	const char *t = b;
-
-	while (*t != '\0' ) {
-		printf(">>>p:%s<<<<\n", p);
-		printf(">>>t:%s<<<<\n", t);
-		printf(">>>pattern:%s<<<<\n", pattern);
-		printf(">>>text:%s<<<<\n", text);
-
+	while (*t != '\0') {
 		if (*p == *t) {
 			p++;
 			t++;
--- src/StringMatchTest.cpp
+++ src/StringMatchTest.cpp
@@ -1,35 +1,35 @@
 #include "gtest/gtest.h"
 #include "BruteForce.h"
 
-TEST(StringMatchTest, should_return_right_position_when_match_pattern_str_by_brute_force_method){
+TEST(StringMatchTest, should_return_right_position_when_match_pattern_str_by_brute_force_method) {
 	BruteForce bf;
 	char text[] = "HERE IS A SIMPLE EXAMPLE", pattern[] = "EXAMPLE";
 
 	ASSERT_EQ(17, bf.match(text, pattern));
 }
 
-TEST(StringMatchTest, should_return_error_when_pattern_str_empty_in_brute_force_method){
+TEST(StringMatchTest, should_return_error_when_pattern_str_empty_in_brute_force_method) {
 	BruteForce bf;
 	char text[] = "HERE IS A SIMPLE EXAMPLE", pattern[] = "";
 
 	ASSERT_EQ(-1, bf.match(text, pattern));
 }
 
-TEST(StringMatchTest, should_return_error_when_text_str_empty_in_brute_force_method){
+TEST(StringMatchTest, should_return_error_when_text_str_empty_in_brute_force_method) {
 	BruteForce bf;
 	char text[] = "", pattern[] = "EXAMPLE";
 
 	ASSERT_EQ(-1, bf.match(text, pattern));
 }
 
-TEST(StringMatchTest, should_return_error_when_text_str_length_less_than_pattern_str_length_in_brute_force_method){
+TEST(StringMatchTest, should_return_error_when_text_str_length_less_than_pattern_str_length_in_brute_force_method) {
 	BruteForce bf;
 	char text[] = "EXAMP", pattern[] = "EXAMPLE";
 
 	ASSERT_EQ(-1, bf.match(text, pattern));
 }
 
-TEST(StringMatchTest, should_return_error_when_pattern_str_not_match_text_str_by_brute_force_method){
+TEST(StringMatchTest, should_return_error_when_pattern_str_not_match_text_str_by_brute_force_method) {
 	BruteForce bf;
 	char text[] = "HERE IS A SIMPLE", pattern[] = "EXAMPLE";
 
